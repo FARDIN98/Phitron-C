20af84e5185428b1e1db7b26a155ff65d5e0524f
FARDIN98 <fardinahmed66@gmail.com>
2022-04-22 15:01:13 +0600

    .

---
+++ Lab_Exam_01/Vowel_Consonent/Vowel_Consonent.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char ch,a,e,i,o,u;
+    scanf("%c", &ch);
+    if (ch == a ||ch == e ||ch == i ||ch == o ||ch == u){
+        printf("vowel");
+    }
+    else{
+        printf("consonent");
+    }
+    return 0;
+    
+}
